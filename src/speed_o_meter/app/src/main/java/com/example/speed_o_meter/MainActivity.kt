9ce308893f355d108ef320bf7a9ef147a1287a51
schef <zvanstefan@gmail.com>
2019-09-25 15:19:07 +0200

    added permission request

--- src/speed_o_meter/app/src/main/java/com/example/speed_o_meter/MainActivity.kt
+++ src/speed_o_meter/app/src/main/java/com/example/speed_o_meter/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.speed_o_meter
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     private var maxSpeed: Float = 0.0f
     private var locationManager: LocationManager? = null
@@ -33,13 +41,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Create persistent LocationManager reference
-        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
-        try {
-            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener);
-        } catch (ex: SecurityException) {
-            Log.d("myTag", "Security Exception, no location available")
-        }
+        startApp(true)
 
 //        fab.setOnClickListener { view ->
 //            try {
@@ -48,6 +50,52 @@ class MainActivity : AppCompatActivity() {
 //                Log.d("myTag", "Security Exception, no location available")
 //            }
 //        }
+    }
 
+
+    private fun startApp(isInitial: Boolean = false) {
+        if (!checkPermissions()) {
+            if (isInitial) {
+                requestPermission()
+            }
+            return
+        }
+        else {
+            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+            try {
+                locationManager?.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    0L,
+                    0f,
+                    locationListener
+                );
+            } catch (ex: SecurityException) {
+                Log.d("myTag", "Security Exception, no location available")
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    private var permissionRequestGranted = false
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionRequestGranted =
+            requestCode == LOCATION_PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        startApp(true)
+    }
+
+    private fun checkPermissions(): Boolean {
+        return if (permissionRequestGranted) true
+        else ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
     }
 }
